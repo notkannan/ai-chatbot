19844e8870cf6819fcdfcf0f8f4d7a2ebad2a37c
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-09 20:24:14 +0530

    Create Payload for OpenAI

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,4 +1,5 @@
-import { ChatGPTMessage } from "@/lib/openai-stream"
+import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt"
+import { ChatGPTMessage, OpenAIStream, OpenAIStreamPayload } from "@/lib/openai-stream"
 import { MessageArraySchema } from "@/lib/validators/message"
 
 export async function POST(req: Request) {
@@ -15,4 +16,20 @@ export async function POST(req: Request) {
         role: 'system',
         content: chatbotPrompt
     })
+
+    const payload: OpenAIStreamPayload = {
+        model: 'gpt-4o-mini',
+        messages: outboundMessages,
+        temperature: 0.4, // How creative the AI will be
+        top_p: 1,
+        frequency_penalty: 0, // How often a certain word will occur
+        presence_penalty: 0, // Value upto 2. Higher the presence penalty, the lesser it will use the words that are already present in the prompt
+        max_token: 150, // Response will be roughly 150 words
+        stream: true, // Required to have a readable stream
+        n: 1
+    }
+
+    const stream = await OpenAIStream(payload)
+
+    return new Response(stream)
 }
