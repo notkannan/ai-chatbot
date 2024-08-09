27ced41312786f9c4d7f6771a40a46f422cd7e9d
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-09 23:35:32 +0530

    Add Chat Messages Component

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -2,6 +2,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 import { FC } from 'react'
 import ChatHeader from './ChatHeader'
 import ChatInput from './ChatInput'
+import ChatMessages from './ChatMessages'
 
 const Chat: FC = ({}) => {
 
@@ -19,6 +20,7 @@ const Chat: FC = ({}) => {
                     </AccordionTrigger>
                     <AccordionContent>
                         <div className="flex flex-col h-80">
+                            <ChatMessages className='px-2 py-3 flex-1'/>
                             <ChatInput className='px-4' />
                         </div>
                     </AccordionContent>
