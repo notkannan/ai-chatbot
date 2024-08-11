dc4aea053ea56188ce713f114147a8883dee1b68
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-08-12 03:06:27 +0530

    Update Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="absolute inset-0 flex justify-center items-center">Website content</main>
+    <main className="absolute inset-0 flex justify-center items-center">Welcome to Northeastern's Intramurals HuskyBot</main>
   );
 }
